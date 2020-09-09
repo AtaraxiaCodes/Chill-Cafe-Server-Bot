bda7b29b4d58d597c3302c9f6e3625ecbd6bd811
Rayhan Arayilakath <rayhnouf@gmail.com>
2020-09-09 14:32:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,170 @@
+const discord = require('discord.js');
+
+const client = new discord.Client();
+
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.end();
+});
+
+app.listen(8080);
+
+//prefix
+const prefix = '%';
+
+// level * levelrate = how much xp you need to get to the next level
+const levelrate = 20;
+
+// How much xp you get from each message
+const msgxp = 1;
+
+function userExists(id) {
+	// Read the file and parse it
+	let data = JSON.parse(fs.readFileSync('data.json'));
+
+	return data[id] != null;
+}
+
+function updateUser(id, profile) {
+	let data = JSON.parse(fs.readFileSync('data.json'));
+
+	data[id] = profile;
+
+	// Update the file
+	fs.writeFileSync('data.json', JSON.stringify(data));
+}
+
+function getUser(id) {
+	let data = JSON.parse(fs.readFileSync('data.json'));
+
+	return data[id];
+}
+
+//Rank up and Level Up Messages
+client.on('message', msg => {
+	if (userExists(msg.author.id)) {
+		let user = getUser(msg.author.id);
+		let elderRole = msg.guild.roles.cache.find(
+			role => role.id == '734189094271320195'
+		);
+		let legendRole = msg.guild.roles.cache.find(
+			role => role.id == '734189143965564939'
+		);
+		let veteranRole = msg.guild.roles.cache.find(
+			role => role.id == '734188797960388628'
+		);
+
+		user.xp += msgxp;
+
+		if (user.xp >= levelrate * user.level) {
+			msg.channel.send(
+				`Congratulations, <@${
+					msg.author.id
+				}>. You leveled up! You are now level ${++user.level}`
+			);
+			user.xp = 0;
+		}
+
+		if (user.level == 5) {
+			msg.channel.send(
+				`Congratulations, <@${
+					msg.author.id
+				}>. You ranked up! You are now <@&734189094271320195>`
+			);
+			msg.member.roles.add(elderRole);
+		}
+
+		if (user.level == 10) {
+			msg.channel.send(
+				`Congratulations, <@${
+					msg.author.id
+				}>. You ranked up! You are now <@&734189143965564939>`
+			);
+			msg.member.roles.add(legendRole);
+		}
+
+		if (user.level == 15) {
+			msg.channel.send(
+				`Congratulations, <@${
+					msg.author.id
+				}>. You ranked up! You are now <@&734188797960388628>`
+			);
+			msg.member.roles.add(veteranRole);
+		}
+
+		updateUser(msg.author.id, user);
+	} else {
+		updateUser(msg.author.id, { xp: 0, level: 1 });
+	}
+
+	//Welcome Message (unstable)
+	client. on('guildMemberAdd', member => {
+		const channel = member.guild.channels.cache.find(
+			ch => ch.name === '⌇😎⌇-public-chat'
+		);
+
+		if (!channel) return;
+
+		channel.send(`Welcome to the server, ${member}`);
+	});
+
+	//General Text Commands
+	if (msg.content == `${prefix}ping`) {
+		msg.channel.send('Pong!'); //Ping for test
+	} else if (msg.content == `${prefix}server`) {
+		msg.channel.send(`This server's name is: ${msg.guild.name}`); //Server Name
+	} else if (msg.content == `${prefix}member`) {
+		msg.channel.send(`Total members: ${msg.guild.memberCount}`); //Member Count
+	} else if (msg.content == `${prefix}created`) {
+		msg.channel.send(`Created On ${msg.guild.createdAt}`); //Creation Date
+	} else if (msg.content == `${prefix}help`) {
+		const helpEmbed = new discord.MessageEmbed()
+			.setColor('#00ffff')
+			.setTitle('Help Documentation')
+			.setDescription('Some Neat Commands')
+			.addFields(
+			  { name: `${prefix}help`, value: 'DMs Help Documentation' },
+				{ name: `${prefix}ping`, value: 'Test Ping' },
+				{ name: `${prefix}server`, value: 'Displays Server Name' },
+				{ name: `${prefix}member`, value: 'Displays Member Count' },
+				{ name: `${prefix}created`, value: 'Displays Creation Date' },
+				{ name: `${prefix}profile`, value: 'Displays Your Profile' },
+	  };
+	}); //Help Documentation
+
+		msg.author.send(helpEmbed);
+	} else if (msg.content == `${prefix}profile`) {
+		const profileEmbed = new discord.MessageEmbed()
+			.setColor('#00ffff')
+			.setTitle('Profile')
+			.setDescription('chill cafe™')
+			.addFields(
+			  { name: `Current Rank`, value: 'Coming Soon' },
+			  { name: `Current Level`, value: 'Coming Soon' },
+			  { name: `EXP to next Level`, value: 'Coming Soon' },
+	  };
+	}); //Profile
+
+		msg.channel.send(profileEmbed);
+  }
+
+const activities_list = [
+	'with the %help command.',
+	'with Node.js',
+	'with the chill cafe™ server',
+	'Shoutout to Star Trek'
+]; // arraylist containing phrases
+
+client.on('ready', () => {
+	setInterval(() => {
+		const index = Math.floor(Math.random() * activities_list.length); // generates a random number between 1 and the length of the activities array list
+		client.user.setActivity('<activity>', { type: 'WATCHING' });
+		client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+	}, 10000); // Runs this every 10 seconds.
+});
+
+client.login(process.env.TOKEN);
